9520ef0a1a249eb378109b7ee5ecd435baad7a05
pranav157 <pranavgupta.v@gmail.com>
2021-08-02 16:57:58 +0200

    progressSteps

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const next = document.getElementById("next");
-const prev = document.getElementById("prev");
-const circles = document.getElementById("circles");
 const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
 
 let currentActive = 1;
 
@@ -11,6 +11,8 @@ next.addEventListener("click", () => {
   if (currentActive > circles.length) {
     currentActive = circles.length;
   }
+
+  update();
 });
 
 prev.addEventListener("click", () => {
@@ -19,23 +21,25 @@ prev.addEventListener("click", () => {
   if (currentActive < 1) {
     currentActive = 1;
   }
+
+  update();
 });
 
 function update() {
-  circles.forEach((circle, id) => {
-    if (id < currentActive) {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
       circle.classList.add("active");
     } else {
       circle.classList.remove("active");
     }
   });
 
-  const active = document.querySelectorAll(".active");
+  const actives = document.querySelectorAll(".active");
 
   progress.style.width =
     ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
 
-  if (currentActive == 1) {
+  if (currentActive === 1) {
     prev.disabled = true;
   } else if (currentActive === circles.length) {
     next.disabled = true;
